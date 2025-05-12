b9660430cc003ac64f9b429747715af86bf90f4a
Jann Stute <jann.stute@student.hpi.uni-potsdam.de>
2025-05-12 15:38:55 +0200

    change get_timestamp to use CLOCK_MONOTONIC_RAW

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <cstdint>
-#include <x86intrin.h>
 #include <thread>
 #include <pthread.h>
 #include <iostream>
+#include <ctime>
 
 #define u8  std::uint8_t
 #define u16 std::uint16_t
@@ -14,11 +14,11 @@
 #define MAX(A, B) (A < B ? B : A)
 
 inline u64 get_timestamp() {
-    // gets a timestamp that is synchronised between cores
-    // uses TSC (other candidate is HPET)
-    // Concerns:
-    // - MUST be an invariant TSC, otherwise TSC might diverge between cores
-    return __rdtsc();
+    // gets a timestamp that is synchronised between cores (ns since unix epoch)
+    // cross core desync should be <400ns in the vast majority of cases
+    timespec ts;
+    assert(clock_gettime(CLOCK_MONOTONIC_RAW, &ts) == 0);
+    return ts.tv_sec * 1000000000 + ts.tv_nsec; // breaks in 2554 CE
 }
 
 inline void set_cpu_affinity(const int cpu, std::thread &thread) {
