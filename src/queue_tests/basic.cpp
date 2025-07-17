3ee159bfee23c471f0415ec229f08eb487377877
Jann Stute <jann.stute@student.hpi.uni-potsdam.de>
2025-07-17 12:40:55 +0200

    feat: cmdline interface to pick queue

--- src/queue_tests/basic.cpp
+++ src/queue_tests/basic.cpp
@@ -1,3 +1,4 @@
+#include <print>
 
 #include "measurer/fine_grained.h"
 #include "queues/b_queue.h"
@@ -7,19 +8,43 @@
 #include "runner.h"
 #include "waiter/continuous.h"
 
-int main() {
-    struct {
-        usize msg_count = 1'000'000;
-        usize consumer_rate = 1'000'000;
-        usize producer_rate = 1'000'000;
-    } params;
+struct {
+    usize msg_count = 1'000'000;
+    usize consumer_rate = 1'000'000;
+    usize producer_rate = 1'000'000;
+} params;
 
-    queues::b_queue bq(16384, 8192, 64, 50);
-    queues::equeue eq(4096, 16384, 50);
-    queues::ff_queue ffq(1024, 16);
-    queues::mc_ring_buffer mcrb(16384, 5000);
-
-    auto q = &mcrb;
-    Runner<std::remove_pointer_t<decltype(q)>, measurer::FineGrained, waiter::Continuous> r(q, params);
+template <typename Q>
+void run_test(Q *queue) {
+    Runner<std::remove_pointer_t<Q>, measurer::FineGrained, waiter::Continuous> r(queue, params);
     r.run();
+}
+
+int main(const int argc, char *argv[]) {
+    if (argc < 2) {
+        std::println("missing queue arg");
+        return EXIT_FAILURE;
+    }
+
+    const auto q_param = std::string(argv[1]);
+    if (q_param == "bq") {
+        std::println(std::cerr, "using bq");
+        queues::b_queue bq(16384, 8192, 64, 50);
+        run_test(&bq);
+    } else if (q_param == "eq") {
+        std::println(std::cerr, "using eq");
+        queues::equeue eq(4096, 16384, 50);
+        run_test(&eq);
+    } else if (q_param == "ffq") {
+        std::println(std::cerr, "using ffq");
+        queues::ff_queue ffq(1024, 16);
+        run_test(&ffq);
+    } else if (q_param == "mcrb") {
+        std::println(std::cerr, "using mcrb");
+        queues::mc_ring_buffer mcrb(16384, 5000);
+        run_test(&mcrb);
+    } else {
+        std::println("invalid queue arg");
+        return EXIT_FAILURE;
+    }
 }
--- src/queue_tests/bursty.cpp
+++ src/queue_tests/bursty.cpp
@@ -1,6 +1,6 @@
-
-
 #include "waiter/bursty.h"
+
+#include <print>
 
 #include "measurer/fine_grained.h"
 #include "queues/b_queue.h"
@@ -9,20 +9,44 @@
 #include "queues/mc_ring_buffer.h"
 #include "runner.h"
 
-int main() {
-    struct {
-        usize msg_count = 1'000'000;
-        usize consumer_rate = 1'000'000;
-        usize producer_rate = 1'000'000;
-        usize burst_size = 2048;
-    } params;
+struct {
+    usize msg_count = 1'000'000;
+    usize consumer_rate = 1'000'000;
+    usize producer_rate = 1'000'000;
+    usize burst_size = 2048;
+} params;
 
-    queues::b_queue bq(16384, 8192, 64, 50);
-    queues::equeue eq(4096, 16384, 50);
-    queues::ff_queue ffq(1024, 16);
-    queues::mc_ring_buffer mcrb(16384, 5000);
-
-    auto q = &mcrb;
-    Runner<std::remove_pointer_t<decltype(q)>, measurer::FineGrained, waiter::Bursty> r(q, params);
+template <typename Q>
+void run_test(Q *queue) {
+    Runner<std::remove_pointer_t<Q>, measurer::FineGrained, waiter::Bursty> r(queue, params);
     r.run();
+}
+
+int main(const int argc, char *argv[]) {
+    if (argc < 2) {
+        std::println("missing queue arg");
+        return EXIT_FAILURE;
+    }
+
+    const auto q_param = std::string(argv[1]);
+    if (q_param == "bq") {
+        std::println(std::cerr, "using bq");
+        queues::b_queue bq(16384, 8192, 64, 50);
+        run_test(&bq);
+    } else if (q_param == "eq") {
+        std::println(std::cerr, "using eq");
+        queues::equeue eq(4096, 16384, 50);
+        run_test(&eq);
+    } else if (q_param == "ffq") {
+        std::println(std::cerr, "using ffq");
+        queues::ff_queue ffq(1024, 16);
+        run_test(&ffq);
+    } else if (q_param == "mcrb") {
+        std::println(std::cerr, "using mcrb");
+        queues::mc_ring_buffer mcrb(16384, 5000);
+        run_test(&mcrb);
+    } else {
+        std::println("invalid queue arg");
+        return EXIT_FAILURE;
+    }
 }
