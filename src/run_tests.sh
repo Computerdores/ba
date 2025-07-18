26de5d4975ba989fcb6957f7f3e60aca0fb1fd87
Jann Stute <jann.stute@student.hpi.uni-potsdam.de>
2025-07-18 12:35:15 +0200

    fix: run_tests.sh

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -5,12 +5,14 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-#sudo cpupower frequency-set -f "2.8G"
+cmake --build build/
+
+sudo cpupower frequency-set -f "2.8G" >/dev/null
 
 for _test in basic bursty; do
     for queue in bq eq ffq; do
-        echo "build/$_test" "$queue" \> "flugzeug_${_test}_${queue}_$1.csv"
+        "build/$_test" "$queue" > "flugzeug_${_test}_${queue}_$1.csv"
     done
 done
 
-#sudo cpupower frequency-set -g powersave
+sudo cpupower frequency-set -g powersave >/dev/null
