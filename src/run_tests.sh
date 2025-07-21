bce92372b628f4830b32bdf76529a9c0ab6e2753
Jann Stute <jann.stute@student.hpi.uni-potsdam.de>
2025-07-21 13:14:52 +0200

    feat: print which test is currently being run

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -10,6 +10,7 @@ cmake --build build/
 sudo cpupower frequency-set -f "2.8G" >/dev/null
 
 for _test in basic bursty; do
+    echo "Now running $_test tests"
     for queue in bq eq ffq; do
         "build/$_test" "$queue" > "flugzeug_${_test}_${queue}_$1.csv"
     done
