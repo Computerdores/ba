a49c92f641c3c2b1602b25369da1f7106084be1b
Jann Stute <jann.stute@student.hpi.uni-potsdam.de>
2025-09-01 13:54:22 +0200

    fix: runner should be closer to real workload

--- src/runner.h
+++ src/runner.h
@@ -10,7 +10,8 @@ using measurer::IsMeasurerPair;
 using queues::IsQueueWith;
 using waiter::IsWaiterPair;
 
-template <IsQueueWith<u64> Q, IsMeasurerPair M, IsWaiterPair W, int CPU_RX = 2, int CPU_TX = 3>
+template <IsQueueWith<u64> Q, IsMeasurerPair M, IsWaiterPair W, bool MEASURE_FAILED = false, int CPU_RX = 2,
+          int CPU_TX = 3>
 class Runner {
   public:
     template <typename P>
@@ -43,6 +44,7 @@ class Runner {
     W _waiter;
     usize _msg_count;
     volatile bool _start = false;
+    CACHE_ALIGNED u64 _data_sink;
 
     void producer() {
         // wait for start
@@ -52,8 +54,9 @@ class Runner {
         _waiter.tx.start();
         for (usize i = 0; i < _msg_count; i++) {
             _measurer.tx.pre();
-            while (!_queue->enqueue(get_timestamp())) {
-                _measurer.tx.pre();
+            auto data = get_timestamp();  // arbitrary data to enqueue
+            while (!_queue->enqueue(data)) {
+                if constexpr (!MEASURE_FAILED) _measurer.tx.pre();
             }
             _measurer.tx.post();
             _waiter.tx.wait();
@@ -68,9 +71,11 @@ class Runner {
         _waiter.rx.start();
         for (usize i = 0; i < _msg_count; i++) {
             _measurer.rx.pre();
-            while (!_queue->dequeue()) {
-                _measurer.rx.pre();
+            std::optional<u64> data = std::nullopt;
+            while (!((data = _queue->dequeue()))) {
+                if constexpr (!MEASURE_FAILED) _measurer.rx.pre();
             }
+            _data_sink += *data;  // do something with the data to force data retrieval from queue
             _measurer.rx.post();
             _waiter.rx.wait();
         }
