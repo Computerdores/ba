bdf44734144454626b4e31cc5fe72b4c001382cf
Jann Stute <jann.stute@student.hpi.uni-potsdam.de>
2025-09-05 14:04:41 +0200

    feat: generate plots from multiple runs

--- src/jupyter/generate_plots.py
+++ src/jupyter/generate_plots.py
@@ -13,27 +13,33 @@ RESULTS_DIR = Path(".")
 
 ERROR_BARS = False
 
-def get_result(benchmark: str, queue: str, preset: str, jitter: bool, all: bool) -> tuple[float, float]:
-    filename = f"data_{preset}_{benchmark}_{queue}_j{str(jitter).lower()}_mf{str(all).lower()}.csv"
-    df = load_results(RESULTS_DIR / filename)
-    # TODO: determine mean and stddev over multiple runs
-    mean = df[["RX_TIME", "TX_TIME"]].mean()
-    std = df[["RX_TIME", "TX_TIME"]].std() / 100 # TODO: this division is only here so the graphs can be worked on in the mean time
-    return (mean["RX_TIME"], std["RX_TIME"]), (mean["TX_TIME"], std["TX_TIME"])
+def get_result(benchmark: str, queue: str, preset: str, jitter: bool, all: bool, runs: int = 100) -> tuple[float, float]:
+    results = []
+    for i in range(runs):
+        filename = f"data_{preset}_{benchmark}_{queue}_j{str(jitter).lower()}_mf{str(all).lower()}_{i+1}.csv"
+        df = load_results(RESULTS_DIR / filename)
+        mean = df[["RX_TIME", "TX_TIME"]].mean()
+        results.append({
+            "queue": queue,
+            "run": i,
+            "RX": mean["RX_TIME"],
+            "TX": mean["TX_TIME"],
+        })
+    return pd.DataFrame(results)
 
 def gen_plot(benchmark: str, queues: list[str], preset: str, jitter: bool, all: bool):
-    results = [(q, get_result(benchmark, q, preset, jitter, all)) for q in queues]
+    results = [get_result(benchmark, q, preset, jitter, all) for q in queues]
+    df = pd.concat(results, ignore_index=True)
 
-    data = [
-        {"queue": q, "type": t, "mean": r[i][0], "std": r[i][1]}
-        for q, r in results
-        for i, t in enumerate(["RX", "TX"])
-    ]
-
-    df = pd.DataFrame(data)
+    df = df.melt(
+        id_vars=["queue", "run"],
+        value_vars=["RX", "TX"],
+        var_name="type",
+        value_name="duration"
+    )
 
     plt.figure(figsize=(8,6))
-    ax: Axes = sns.barplot(data=df, x="queue", y="mean", hue="type", errorbar=None, capsize=0.1)
+    ax: Axes = sns.barplot(data=df, x="queue", y="duration", hue="type", errorbar="sd", capsize=0.1)
 
     if ERROR_BARS:
         for i, row in df.iterrows():
@@ -52,6 +58,8 @@ def gen_plot_quad(benchmark: str, queues: list[str], preset: str):
     for jitter in [True, False]:
         for all in [True, False]:
             gen_plot(benchmark, queues, preset, jitter, all)
+            print(f"{benchmark} {jitter} {all} - done")
 
 if __name__ == "__main__":
-    gen_plot_quad("bursty", ["bq", "eq", "fflwq", "ffwdq", "lprt"], "5157004_16k")
+    gen_plot_quad("basic", ["bq", "eq", "fflwq", "ffwdq", "lprt"], "3189a2f_2025-09-04")
+    gen_plot_quad("bursty", ["bq", "eq", "fflwq", "ffwdq", "lprt"], "3189a2f_2025-09-04")
